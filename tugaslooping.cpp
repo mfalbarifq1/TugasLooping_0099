91db5fec354eef03b700bd5afc215ca6ab9bd9d7
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-31 19:22:41 +0700

    if else fibonacci

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -45,4 +45,10 @@ void tampilHasilPrima(){
     }
 }
 
-void tampilHasilFibonacci()
+void tampilHasilFibonacci(){
+    if (isFibonacci(angka)) {
+        cout << angka << " termasuk bilangan Fibonacci.\n";
+    } else {
+        cout << angka << " bukan bilangan Fibonacci.\n";
+    }
+}
