8d56aae0a35b7db237e0c0183c5cc2ecdeb703e4
mfalbarifq1 <mfalbarif.q1@gmail.com>
2026-03-31 18:55:44 +0700

    pengecekan return true

--- tugaslooping.cpp
+++ tugaslooping.cpp
@@ -7,4 +7,13 @@ int pilihan;
 
 bool isPrima(int n){
     if (n <= 1) return false;
+     int i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+        }
+        i++;
+    }
+    return true;
+
 }
